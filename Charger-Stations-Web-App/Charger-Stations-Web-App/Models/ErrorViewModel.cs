950d2ed792fd1048bfc21b69315e632023167801
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2023-09-14 11:56:02 +0300

    First Commit. Basic Logic

---
+++ Charger-Stations-Web-App/Charger-Stations-Web-App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Charger_Stations_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
